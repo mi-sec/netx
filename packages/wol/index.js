fbf6c7541bc3421d1cf0856c80a6b4ee4c7ef9d9
iskore <nicksoggin@gmail.com>
2020-03-09 23:19:51 -0400

    wakeOnLan first draft complete

--- packages/wol/index.js
+++ packages/wol/index.js
@@ -15,8 +15,7 @@ export const MAGIC_PACKET_MAC_COPIES = 16;
 export function createMagicPacket( mac ) {
 	mac = macAddress( mac );
 
-	const
-		buf = Buffer.alloc( ( 1 + MAGIC_PACKET_MAC_COPIES ) * MAC_BYTES );
+	const buf = Buffer.alloc( ( 1 + MAGIC_PACKET_MAC_COPIES ) * MAC_BYTES );
 
 	buf.fill( 0xFF, 0, MAC_BYTES );
 
@@ -25,4 +24,49 @@ export function createMagicPacket( mac ) {
 	}
 
 	return buf;
+}
+
+export default async function wakeOnLan( mac, opts = {} ) {
+	opts.address  = opts.address || '255.255.255.255';
+	opts.packets  = opts.packets || 3;
+	opts.interval = opts.interval || 100;
+	opts.port     = opts.port || 9;
+
+	const
+		magicPacket = createMagicPacket( mac ),
+		socket      = dgram.createSocket( net.isIPv6( opts.address ) ? 'udp6' : 'udp4' );
+
+	function wait( t ) {
+		return new Promise(
+			res => setTimeout( () => res(), t )
+		);
+	}
+
+	function sendPacket() {
+		return new Promise(
+			( res, rej ) => socket.send(
+				magicPacket, 0,
+				magicPacket.length,
+				opts.port,
+				opts.address,
+				err => err ? rej( err ) : res()
+			)
+		);
+	}
+
+	socket.on( 'error', sendPacket );
+	socket.once( 'listening', () => socket.setBroadcast( true ) );
+
+	for ( let i = 0; i < opts.packets; i++ ) {
+		await sendPacket( i === opts.packets );
+		await wait( opts.interval );
+	}
+
+	socket.close();
+
+	return {
+		status: 'sent',
+		mac,
+		...opts
+	};
 }
