1916e1f3b89508a5b46918dcede36e1d75ab8de2
iskore <nicksoggin@gmail.com>
2020-05-14 23:22:58 -0400

    @mi-sec/ip v1

--- packages/ip/lib/ip.js
+++ packages/ip/lib/ip.js
@@ -5,14 +5,53 @@
  *******************************************************************************************************/
 'use strict';
 
-function sum( x, y ) {
-	return x + y;
+function validIPv4( ip ) {
+	if ( ip === 'localhost' ) {
+		return true;
+	}
+
+	return /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/
+		.test( ip );
 }
 
-function subtract( x, y ) {
-	return x - y;
+function longToIp( long ) {
+	return [
+		( long & ( 0xFF << 24 ) ) >>> 24,
+		( long & ( 0xFF << 16 ) ) >>> 16,
+		( long & ( 0xFF << 8 ) ) >>> 8,
+		long & 0xFF
+	].join( '.' );
+}
+
+function ipToLong( ip ) {
+	if ( ip === 'localhost' ) {
+		return 0x7F000001;
+	}
+
+	const b = ( ip + '' ).split( '.' );
+
+	if ( b.length === 0 || b.length > 4 ) {
+		throw new Error( 'invalid ip' );
+	}
+
+	for ( let i = 0; i < b.length; i++ ) {
+		const byte = +b[ i ];
+
+		if ( byte !== byte || byte < 0 || byte > 255 ) {
+			throw new Error( `invalid byte: ${ byte }` );
+		}
+	}
+
+	return (
+		( b[ 0 ] || 0 ) << 24 |
+		( b[ 1 ] || 0 ) << 16 |
+		( b[ 2 ] || 0 ) << 8 |
+		( b[ 3 ] || 0 )
+	) >>> 0;
 }
 
 module.exports = {
-	sum, subtract
+	validIPv4,
+	longToIp,
+	ipToLong
 };
--- packages/ip/test/ip.test.js
+++ packages/ip/test/ip.test.js
@@ -1,6 +1,6 @@
 /** ****************************************************************************************************
  * File: mac-address.test.js
- * Project: @mi-sec/mac-address
+ * Project: @mi-sec/ip
  * @author MI-SEC
  *******************************************************************************************************/
 'use strict';
@@ -12,8 +12,27 @@ const
 const ip = require( '../lib/ip' );
 
 describe( `${ process.env.npm_package_name } v${ process.env.npm_package_version }`, function () {
-	it( 'should sum numbers', () => {
-		expect( ip.sum( 1, 2 ) ).to.eq( 3 );
-		expect( ip.subtract( 4, 1 ) ).to.eq( 3 );
+	it( 'NetworkCidr.longToIp should convert long to ip string', () => {
+		// FF.FF.FF.FF
+		expect( ip.longToIp( 4294967295 ) )
+			.to.be.a( 'string' )
+			.to.equal( '255.255.255.255' );
+
+		// C0.A8.01.01
+		expect( ip.longToIp( 3232235777 ) )
+			.to.be.a( 'string' )
+			.to.equal( '192.168.1.1' );
+	} );
+
+	it( 'NetworkCidr.ipToLong should convert ip to long number', () => {
+		// FF.FF.FF.FF
+		expect( ip.ipToLong( '255.255.255.255' ) )
+			.to.be.a( 'number' )
+			.to.equal( 4294967295 );
+
+		// C0.A8.01.01
+		expect( ip.ipToLong( '192.168.1.1' ) )
+			.to.be.a( 'number' )
+			.to.equal( 3232235777 );
 	} );
 } );
