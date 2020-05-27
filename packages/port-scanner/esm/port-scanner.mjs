3e39b8eb8b3b582b3809b50cab3f69128eeb9e73
iskore <nicksoggin@gmail.com>
2020-05-27 08:38:51 -0400

    port-scanner esm build

--- packages/port-scanner/esm/port-scanner.mjs
+++ packages/port-scanner/esm/port-scanner.mjs
@@ -3,6 +3,7 @@ import mod from "../lib/port-scanner.js";
 export default mod;
 export const isRange = mod.isRange;
 export const portRangeIterator = mod.portRangeIterator;
+export const PortScanner = mod.PortScanner;
+export const TCPConnect = mod.TCPConnect;
 export const commonPorts = mod.commonPorts;
 export const convertHighResolutionTime = mod.convertHighResolutionTime;
-export const connect = mod.connect;
