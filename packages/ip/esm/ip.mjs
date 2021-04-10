16a48eacb9d7e3c353342fc1ee294832cfeeecc7
Nick Soggin <nicksoggin@gmail.com>
2021-04-09 22:19:48 -0400

    updated all modules

--- packages/ip/esm/ip.mjs
+++ packages/ip/esm/ip.mjs
@@ -1,6 +1,6 @@
 import mod from "../lib/ip.js";
 
 export default mod;
-export const validIPv4 = mod.validIPv4;
-export const longToIp = mod.longToIp;
 export const ipToLong = mod.ipToLong;
+export const longToIp = mod.longToIp;
+export const validIPv4 = mod.validIPv4;
--- packages/port-scanner/esm/port-scanner.mjs
+++ packages/port-scanner/esm/port-scanner.mjs
@@ -1,9 +1,9 @@
 import mod from "../lib/port-scanner.js";
 
 export default mod;
-export const isRange = mod.isRange;
-export const portRangeIterator = mod.portRangeIterator;
 export const PortScanner = mod.PortScanner;
 export const TCPConnect = mod.TCPConnect;
 export const commonPorts = mod.commonPorts;
 export const convertHighResolutionTime = mod.convertHighResolutionTime;
+export const isRange = mod.isRange;
+export const portRangeIterator = mod.portRangeIterator;
